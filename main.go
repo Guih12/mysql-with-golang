1ce6b7ef6a7de322a3b47f3de38f0e4d5ef5b136
George Borsato <georginhoborsato@gmail.com>
2022-01-24 13:01:40 -0300

    feat: get all users

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/users", server.CreateUser).Methods(http.MethodPost)
+	router.HandleFunc("/users", server.GetAllUsers).Methods(http.MethodGet)
 
 	log.Fatal(http.ListenAndServe(":5000", router))
 }
--- server/server.go
+++ server/server.go
@@ -51,8 +51,45 @@ func CreateUser(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("1"))
 		return
 	}
-	id, err := insert.LastInsertId()
+	id, _ := insert.LastInsertId()
 
 	w.WriteHeader(http.StatusCreated)
 	w.Write([]byte(fmt.Sprintf("Usuário inserido com sucesso id: %d", id)))
+}
+
+func GetAllUsers(w http.ResponseWriter, r *http.Request) {
+	db, err := banco.Connect()
+
+	if err != nil {
+		w.Write([]byte("Erro ao conectar"))
+	}
+
+	defer db.Close()
+
+	rows, erro := db.Query("select * from users")
+
+	if erro != nil {
+		w.Write([]byte("Erro ao buscar usuários"))
+	}
+
+	defer rows.Close()
+
+	var usuarios []user
+
+	for rows.Next() {
+		var usuario user
+
+		if erro := rows.Scan(&usuario.ID, &usuario.Name, &usuario.Email); erro != nil {
+			w.Write([]byte("Erro ao scanear"))
+			return
+		}
+		usuarios = append(usuarios, usuario)
+	}
+
+	w.WriteHeader(http.StatusOK)
+	if erro := json.NewEncoder(w).Encode(usuarios); erro != nil {
+		w.Write([]byte("Erro ao conveter"))
+		return
+	}
+
 }
